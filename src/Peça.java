185dcd301b409d2ce112ff572f5d1057386d67c4
IgorItiroko <igorshinji@yahoo.com>
2021-11-18 09:30:33 -0300

    Peça feita

--- src/Peça.java
+++ src/Peça.java
@@ -1,4 +1,32 @@
 
 public class peça {
+	private boolean viva = true;
+	private boolean branca = false;
 	
+	public Piece(boolean branca)
+	{
+		this.setBranca(branca);
+	}
+	
+	public boolean getBranca()
+	{
+		return this.isBranca;
+	}
+	
+	public void setBranca(boolean branca)
+	{
+		this.branca = branca;
+	}
+	
+	public boolean getViva()
+	{
+		return this.viva;
+	}
+	
+	public void setViva(boolean viva)
+	{
+		this.viva = viva;
+	}
+	
+	public abstract boolean canMove(Tabuleiro tabuleiro,Quadrado inicio,Quadrado fim);
 }
